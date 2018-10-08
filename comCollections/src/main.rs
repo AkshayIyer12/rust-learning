efabba537aee9da5356b8fd361b6a765563b0d2d
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-10-08 11:15:24 +0530

    Add spreadsheet example

--- comCollections/src/main.rs
+++ comCollections/src/main.rs
@@ -7,9 +7,19 @@ fn main() {
     //     Some(_) => { println!("Reachable element at index: {}", v_index); },
     //     None => { println!("Unreachable element at index: {}", v_index); }
     // }
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
+    // let mut v = vec![100, 32, 57];
+    // for i in &mut v {
+    //     *i += 50;
+    // }
+    // println!("Hello, world!, {:?}", v);
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String)
     }
-    println!("Hello, world!, {:?}", v);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(10.12),
+        SpreadsheetCell::Text(String::from("blue"))
+    ];
 }
