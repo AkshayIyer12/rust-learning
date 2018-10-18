c52d760980ea10f37a9314ab362a495d3814d3d9
AkshayIyer12 <c.m.akshay.iyer@gmail.com>
2018-10-18 09:12:25 +0530

    Refactor error handlers

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,15 +2,13 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
-    let f = match f {
-    	Ok(file) => file,
-	Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-	        Ok(fc) => fc,
-		Err(e) => panic!("Tried to create file, but there was a problem {:?}", e)
- 	    },
-	    other_error => panic!("There was a problem in opening the file {:?}", other_error)
+    let f = File::open("hello.txt").map_err(|error| {
+    	if error.kind() == ErrorKind::NotFound {
+	   File::create("hello.txt").unwrap_or_else(|error| {
+	       panic!("Tried to create file, but there was a problem {:?}", error);
+	   })
+	} else {
+	  panic!("There was a problem in opening the file {:?}", error);
 	}
-    };
+    });
 }
