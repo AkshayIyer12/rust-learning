6b1e4f7b11f7212b9946c6fd0a5d4c7c6391f4f4
AkshayIyer12 <c.m.akshay.iyer@gmail.com>
2018-10-22 20:26:11 +0530

    Create havoc in main code

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,14 +1,11 @@
-use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
 use std::fs::File;
 
+// fn read_username_from_file() -> Result<String, io::Error> {
+//   fs::read_to_string("hello.txt")
+// }
 fn main() {
-    let f = File::open("hello.txt").map_err(|error| {
-    	if error.kind() == ErrorKind::NotFound {
-	   File::create("hello.txt").unwrap_or_else(|error| {
-	       panic!("Tried to create file, but there was a problem {:?}", error);
-	   })
-	} else {
-	  panic!("There was a problem in opening the file {:?}", error);
-	}
-    });
+// let _v: Result<String, io::Error> = read_username_from_file();
+   let f = File::open("hello.txt")?;
 }
