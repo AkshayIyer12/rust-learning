eff8528980d2a894f896308d630c7e481df6a251
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-11-06 22:35:05 +0530

    Add generics, traits, smart pointers, closures and iterators

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,23 +1,9 @@
-fn largest<T>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Point<T> {
+    x: T,
+    y: T
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+   let _integer = Point { x: 5, y: 10 };
+   let _float = Point { x: 5.0, y: 10.0 };
 }
---
+++ smart_pointers/src/main.rs
@@ -0,0 +1,67 @@
+// use self::Lists::{Cons, Nil};
+// use std::ops::Deref;
+// use std::boxed::Box;
+
+// enum Lists {
+//     Cons(i32, Box<Lists>),
+//     Nil
+// }
+// struct MyBox<T>(T);
+
+// impl<T> Deref for MyBox<T> {
+//     type Target = T;
+
+//     fn deref(&self) -> &T {
+//         &self.0
+//     }
+    
+//     fn new(x: T) -> MyBox<T> {
+//         MyBox(x)
+//     }
+// }
+// fn main() {
+//     let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+//     let x = 5;
+//     let y = MyBox::new(x);
+
+//     assert_eq!(5, x);
+//     assert_eq!(5, *y);
+// }
+// use std::mem::drop;
+// struct CustomSmartPointer {
+//     data: String,
+// }
+
+// impl Drop for CustomSmartPointer {
+//     fn drop(&mut self) {
+//         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+//     }
+// }
+
+// fn main() {
+//     let a = CustomSmartPointer { data: String::from("my stuff 1") };
+//     let b = CustomSmartPointer { data: String::from("other stuff 2") };
+//     let c = CustomSmartPointer { data: String::from("my stuff 3") };
+//     let d = CustomSmartPointer { data: String::from("other stuff 4") };
+//     drop(c);
+//     println!("CustomSmartPointers created.");
+// }
+
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil
+// }
+// use self::List::{Cons, Nil};
+// use std::rc::Rc;
+
+// fn main() {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Cons(3, Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//     {
+//         let c = Cons(4, Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//      println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+// }
