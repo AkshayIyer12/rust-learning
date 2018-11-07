caefc3229c9820c4943a62b60f0ddbf334610fc6
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-11-07 19:22:12 +0530

    Overflow the stack

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -46,28 +46,69 @@
 //     drop(c);
 //     println!("CustomSmartPointers created.");
 // }
-#[derive(Debug)]
-enum List {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
-    Nil
-}
+// #[derive(Debug)]
+// enum List {
+//     Cons(Rc<RefCell<i32>>, Rc<List>),
+//     Nil
+// }
 
+// use self::List::{Cons, Nil};
+// use std::rc::Rc;
+// use std::cell::RefCell;
+
+// fn main() {
+//     let value = Rc::new(RefCell::new(5));
+//     let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+//     println!("count after creating a = {}", Rc::strong_count(&a));
+//     let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+//     println!("count after creating b = {}", Rc::strong_count(&a));
+//     {
+//         let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+//         println!("count after creating c = {}", Rc::strong_count(&a));
+//     }
+//     *value.borrow_mut() *= 10;
+//     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+//     println!("a after = {:?}", a);
+//     println!("b after = {:?}", b);
+// }
 use self::List::{Cons, Nil};
 use std::rc::Rc;
 use std::cell::RefCell;
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
 
 fn main() {
-    let value = Rc::new(RefCell::new(5));
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-    println!("count after creating a = {}", Rc::strong_count(&a));
-    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
-    println!("count after creating b = {}", Rc::strong_count(&a));
-    {
-        let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
-        println!("count after creating c = {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
     }
-    *value.borrow_mut() *= 10;
-    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    println!("a next item = {:?}", a.tail());
 }
