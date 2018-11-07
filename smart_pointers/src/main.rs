7d17b3a2b6ac9e5264fe4309c88396f672953213
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-11-07 16:21:40 +0530

    Add more example for interior mutability pattern

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -46,25 +46,28 @@
 //     drop(c);
 //     println!("CustomSmartPointers created.");
 // }
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil
+}
 
-// enum List {
-//     Cons(i32, Rc<List>),
-//     Nil
-// }
-// use self::List::{Cons, Nil};
-// use std::rc::Rc;
+use self::List::{Cons, Nil};
+use std::rc::Rc;
+use std::cell::RefCell;
 
-// fn main() {
-//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-//     println!("count after creating a = {}", Rc::strong_count(&a));
-//     let b = Cons(3, Rc::clone(&a));
-//     println!("count after creating b = {}", Rc::strong_count(&a));
-//     {
-//         let c = Cons(4, Rc::clone(&a));
-//         println!("count after creating c = {}", Rc::strong_count(&a));
-//     }
-//      println!("count after c goes out of scope = {}", Rc::strong_count(&a));
-// }
 fn main() {
-    println!("hello world");
+    let value = Rc::new(RefCell::new(5));
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    *value.borrow_mut() *= 10;
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
 }
