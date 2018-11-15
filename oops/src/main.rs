962d60a020b1e0877714ea7963b2db7800623582
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-11-15 15:45:58 +0530

    More patterns with static and dynamic dispatch

--- oops/src/main.rs
+++ oops/src/main.rs
@@ -1,34 +1,34 @@
-pub struct AveragedCollection {
-    list: Vec<i32>,
-    average: f64
+use gui::{Screen, Button};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>
 }
 
-impl AveragedCollection {
-    pub fn add(&mut self, value: i32) {
-        self.list.push(value);
-        self.update_average();
-    }
-
-    pub fn remove(&mut self) -> Option<i32> {
-        let result = self.list.pop();
-        match result {
-            Some(value) => {
-                self.update_average();
-                Some(value)
-            },
-            None => None
-        }
-    }
-
-    pub fn average(&self) -> f64 {
-        self.average
-    }
-
-    fn update_average(&mut self) {
-        let total: i32 = self.list.iter().sum();
-        self.average = total as f64 / self.list.len() as f64;
+impl Draw for SelectBox {
+    fn draw(&self) {
+        //
     }
 }
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            })
+        ]
+    }
 }
