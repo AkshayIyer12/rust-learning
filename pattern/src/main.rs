48568868c971c55813a0f065479c35e251bfa267
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-11-27 22:00:35 +0530

    Add more stuff from pattern matching

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -1,19 +1,62 @@
-fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = "34".parse();
+struct Point {
+    x: i32,
+    y: i32
+}
+// enum Color {
+//    Rgb(i32, i32, i32),
+//    Hsv(i32, i32, i32)
+// }
 
-    if let Some(color) = favorite_color {
-        println!("Using your favorite color, {}, as the background", color);
-    } else if is_tuesday {
-        println!("Tuesday is green day!");
-    } else if let Ok(age) = age {
-        if age > 30 {
-            println!("Using purple as the background color");
-        } else {
-            println!("Using orange as the background color");
-        }
-    } else {
-        println!("Using blue as the background color");
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(Color),
+// }
+
+// fn main() {
+//     let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+//     match msg {
+//         Message::ChangeColor(Color::Rgb(r, g, b)) => {
+//             println!(
+//                 "Change the color to red {}, green {}, and blue {}",
+//                 r,
+//                 g,
+//                 b
+//             )     
+//         },
+//         Message::ChangeColor(Color::Hsv(h, s, v)) => {
+//             println!(
+//                 "Change the color to hue {}, saturation {}, and value {}",
+//                 h,
+//                 s,
+//                 v
+//             )
+//         }
+//         _ => ()
+//     }
+// }
+
+// fn main () {
+//     let points = vec![
+//         Point { x: 0, y: 0 },
+//         Point { x: 1, y: 5 },
+//         Point { x: 10, y: -3 }
+//     ];
+
+//     let sum_of_squares: i32 = points.iter().map(|&Point  { x, y }| x * x + y * y).sum();
+// }
+
+fn main() {
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(x) => println!("Got 50 {}", x),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
     }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
