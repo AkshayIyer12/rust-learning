80da180e7d3a1f4a9eb00262ef224679ff05f78e
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2018-12-06 12:34:41 +0530

    Add graceful shutdown of workers
    - Send a message to them that break the endless loop
    - Call join on thread to end them

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,7 +5,7 @@ use std::sync::Mutex;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>
+    sender: mpsc::Sender<Message>
 }
 
 trait FnBox {
@@ -51,30 +51,64 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+        
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>
+    thread: Option<thread::JoinHandle<()>>
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
+                let message  = receiver.lock().unwrap().recv().unwrap();
+                match message {
+                    Message::NewJob(job) => {
+                         println!("Worker {} got a job; executing.", id);
 
-                println!("Worker {} got a job; executing.", id);
-
-                job.call_box();
+                        job.call_box();
+                    },
+                    Message::Terminate => {
+                        println!("Worker {} was told to terminate.", id);
+                        break;
+                    }
+                }
+               
             }
         });
 
         Worker {
             id,
-            thread
+            thread: Some(thread)
         }
     }
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate
 }
